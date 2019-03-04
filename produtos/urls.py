3881f2fbf17ff8303564834f2080660491135b3b
Kenio Natan <kenionatan@gmail.com>
2019-03-04 17:25:43 -0300

    Update product

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import product_list, product_new
+from .views import product_list, product_new, product_update
 
 urlpatterns = [
     path('list/', product_list, name="product_list"),
-    path('new/', product_new, name="product_new")
+    path('new/', product_new, name="product_new"),
+    path('update/<int:id>/', product_update, name="product_update")
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from .forms import ProductForm
 
@@ -10,6 +10,16 @@ def product_list(request):
 
 def product_new(request):
     form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('product_list')
+    return render(request, 'new_product.html', {'form': form})
+
+
+def product_update(request, id):
+    product = get_object_or_404(Product, pk=id)
+    form = ProductForm(request.POST or None, instance=product)
 
     if form.is_valid():
         form.save()
