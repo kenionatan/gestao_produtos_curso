2e0e97b1f644a49d6ddc6f2a146f27aa09546792
Kenio Natan <kenionatan@gmail.com>
2019-03-04 18:07:18 -0300

    Deploy to Heroku

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gestao-pedidos-backend.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'gestao-produtos-kenio.herokuapp.com']
 
 
 # Application definition
